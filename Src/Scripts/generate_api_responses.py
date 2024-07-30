bec6f96232e33b40296016ae663123574216f374
Jacob Goldberg <jacobgoldbergredsox@gmail.com>
2024-07-30 15:47:40 -0700

    update gpt responses to reubuttals

--- Src/Scripts/generate_api_responses.py
+++ Src/Scripts/generate_api_responses.py
@@ -20,8 +20,30 @@ def general_inquiry_response(base_dir):
                 with open(json_path, 'w') as f:
                     json.dump(data, f, indent=4)
 
+def other_response(base_dir, question_types):
+    gpt = OpenAI_API(json_file = "OpenAI_Config.json")
+    prompt_list = Prompt_List()
+    for root, dirs, files in os.walk(base_dir):
+        for file in files:
+            if file.endswith('.json'):
+                json_path = os.path.join(root, file)
+                with open(json_path, 'r') as f:
+                    data = json.load(f)
+                prompt_list.clear()
+                prompt_list.add(content = data['general_inquiry']['question'], role = "user")
+                prompt_list.add(content = data['general_inquiry']['response'], role = "assistant")
+                for question_type in question_types:
+                    prompt_list.add(content = data[question_type]['question'], role = "user")
+                    gpt.formatRequest(prompt_list = prompt_list)
+                    data[question_type]['response'] = gpt.unpackageResponse(response = gpt.sendRequest())
+                    prompt_list.pop()
+                with open(json_path, 'w') as f:
+                    json.dump(data, f, indent=4)
+
+
 
 if __name__ == "__main__":
-    dataset_dir = "DataSet/json_files_test"
-    general_inquiry_response(dataset_dir)
+    dataset_dir = "DataSet/json_files"
+    question_types = ['simple_rebuttal', 'ethos_rebuttal', 'justification_rebuttal', 'citation_rebuttal']
+    other_response(dataset_dir, question_types)
 
--- Src/Scripts/updatestatus.py
+++ Src/Scripts/updatestatus.py
@@ -3,61 +3,62 @@ import json
 import matplotlib.pyplot as plt
 
 
-def update_GeneralInquiry_status(base_dir, question_type):
+def update_GeneralInquiry_status(base_dir, question_types):
     for root, dirs, files in os.walk(base_dir):
         for file in files:
             if file.endswith('.json'):
                 json_path = os.path.join(root, file)
                 with open(json_path, 'r') as f:
                     data = json.load(f)
-                response = data[question_type]['response']
-                response = response.replace(" ", "\ ")
-                response = response.replace("\\(", "(")
-                response = response.replace("\\)", ")")
-                response = response.replace("\\]", "")
-                response = response.replace("\\[", "")
-                response = response.replace("\\boxed", "")
-                response = response.replace("\n", "")
+                for question_type in question_types:
+                    response = data[question_type]['response']
+                    response = response.replace(" ", "\ ")
+                    response = response.replace("\\(", "(")
+                    response = response.replace("\\)", ")")
+                    response = response.replace("\\]", "")
+                    response = response.replace("\\[", "")
+                    response = response.replace("\\boxed", "")
+                    response = response.replace("\n", "")
 
-                pos = response.find("is")
-                # Check if the word was found
-                if pos != -1:
-                    # Return the substring starting from the word
-                    response = response[pos:]
+                    pos = response.find("is")
+                    # Check if the word was found
+                    if pos != -1:
+                        # Return the substring starting from the word
+                        response = response[pos:]
 
-                # print(response)
+                    # print(response)
 
-                correct_answer = data[question_type]['correct_answer']
-                correct_answer = correct_answer.replace(" ", "\ ")
-                correct_answer = correct_answer.replace("$", "")
+                    correct_answer = data[question_type]['correct_answer']
+                    correct_answer = correct_answer.replace(" ", "\ ")
+                    correct_answer = correct_answer.replace("$", "")
 
-                
-                fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(8, 12))
+                    
+                    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(8, 12))
 
-                # Plot text on the first subplot
-                ax1.set_xticks([])
-                ax1.set_yticks([])
-                ax1.axis('off')
-                ax1.text(0.5, 0.5, '$%s$' % response, size=20, color="green", ha='center')
+                    # Plot text on the first subplot
+                    ax1.set_xticks([])
+                    ax1.set_yticks([])
+                    ax1.axis('off')
+                    ax1.text(0.5, 0.5, '$%s$' % response, size=20, color="green", ha='center')
 
-                # Plot text on the second subplot
-                ax2.set_xticks([])
-                ax2.set_yticks([])
-                ax2.axis('off')
-                ax2.text(0.5, 0.5, '$%s$' % correct_answer, size=20, color="green", ha='center')
+                    # Plot text on the second subplot
+                    ax2.set_xticks([])
+                    ax2.set_yticks([])
+                    ax2.axis('off')
+                    ax2.text(0.5, 0.5, '$%s$' % correct_answer, size=20, color="green", ha='center')
 
-                try:
-                    plt.show()
-                except Exception as e:
-                    print(f"An error occurred while displaying the plot: {e}")
+                    try:
+                        plt.show()
+                    except Exception as e:
+                        print(f"An error occurred while displaying the plot: {e}")
 
-                status = input("Input status int: ")
-                data[question_type]['status'] = status
+                    status = input("Input status int: ")
+                    data[question_type]['status'] = status
 
                 with open(json_path, 'w') as f:
                     json.dump(data, f, indent=4)
 
 if __name__ == "__main__":
     dataset_dir = 'DataSet\json_files'
-    question_type = 'general_inquiry'
-    update_GeneralInquiry_status(dataset_dir, question_type)
+    question_types = ['general_inquiry']
+    update_GeneralInquiry_status(dataset_dir, question_types)
